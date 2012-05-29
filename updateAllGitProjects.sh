0a0a155e3824fa510b91e41e4755434331ceb82c
David Godfrey <reactiveraven@gmail.com>
2012-05-29 13:47:26 +0100

    Adding uagp script

---
+++ updateAllGitProjects.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+pushd $DIR >/dev/null 2>&1;
+
+find . -maxdepth 2 -name ".git" |
+tr "/" "\n" | grep -v "^\.$" | grep -v "^\.git$" |
+while read line;
+do
+	pushd "$line" > /dev/null 2>&1;
+	echo;
+	echo "==========";
+	echo $line;
+	echo "==========";
+	branch="`git branch | grep "^*" | cut -c 3- | tr -d " \n"`";
+	git pull --ff origin $branch;
+	popd > /dev/null 2>&1;
+done;
+
+popd > /dev/null 2>&1;
